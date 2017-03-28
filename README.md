422a322d6701db36d28420aa46abd36de142a63c
CoolAcid <coolacid@gmail.com>
2017-03-28 13:26:09 -0400

    Readme?

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # BadCertCheck
 Check for possible EV Cert Problems
+
+Google has announced their intention to de-trust EV certs from Symantec CAs [See Here](https://groups.google.com/a/chromium.org/forum/m/#!msg/blink-dev/eUAKwjihhBs/rpxMXjZHCQAJ)
+
+This tool is a simple check to pull the CA chain from a domain and check to see if any of the certs are listed to be de-trusted. 
+
+You can find the list of certs to be de-trusted [here](https://chromium.googlesource.com/chromium/src/+/master/net/data/ssl/symantec/roots/)
+
+USAGE:
+
+`python checker.py [HOSTNAME]`
+
+NOTE: Use the hostname, not URL
+
---
+++ checker.py
@@ -0,0 +1,37 @@
+import M2Crypto
+import roots
+from optparse import OptionParser
+
+usage = "Usage: %prog HOSTNAME"
+parser = OptionParser(usage)
+
+# Basis for this from https://gist.github.com/zakird/1346293
+
+(options, args) = parser.parse_args()
+
+if len(args) != 1:
+    print "HOSTNAME not supplied, using google"
+    address = "www.google.com"
+else:
+    address = args[0]
+
+
+context = M2Crypto.SSL.Context();
+context.set_allow_unknown_ca(True)
+context.set_verify(M2Crypto.SSL.verify_none, True)
+
+conn = M2Crypto.SSL.Connection(context)
+conn.connect((address, 443))
+
+cert_chain = conn.get_peer_cert_chain()
+badcert = False
+
+for cert in cert_chain:
+    print "%s: %s" % (cert.get_subject().as_text(), cert.get_fingerprint("sha256"))
+    if cert.get_fingerprint("sha256").lower() in roots.roots:
+        badcert = True
+
+if badcert:
+    print "\n\nKNOWN BAD CERT IN THE CHAIN"
+
+
